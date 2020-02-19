655059e36d0ad64170408682f4b739165e48708d
Erraih <rabieerraih@gmail.com>
2020-02-19 17:41:10 +0100

    typo route

--- routes/api.php
+++ routes/api.php
@@ -39,5 +39,5 @@ Route::post('/user', 'UserController@store');
 Route::put('/user/{id}', 'UserController@update');
 Route::delete('/user/{id}', 'UserController@delete');
 
-Route::get('/score', 'ScoreController@getAllScores');
-Route::post('/score', 'ScoreController@store');
+Route::get('/user/score', 'ScoreController@getAllScores');
+Route::post('/user/score', 'ScoreController@store');
